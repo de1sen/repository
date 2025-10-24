796a52b8cbe77dbb2c6103a815eff4c363b9c0aa
de1sen <EgorShparovv@yandex.ru>
2025-10-24 16:44:38 +0300

    print function

--- stack1.c
+++ stack1.c
@@ -9,11 +9,21 @@ typedef struct {
     unsigned int n;  // сколько элементов хранится в стеке
 } Stack;
 
+void print(Stack * st)
+{
+    for (int i = 0; i  < st->n; i++)
+        printf("%d ", st->a[i]);
+    printf("\n");
+}
+
 int main(void)
 {
     Stack s = {{7,4,1}, 3};
 
     Stack * st = &s;
 
+    print(st);
+
     return 0;
 }
+
