bf9869c6ee1e7ca3f6d08f01c86dc835e531001f
de1sen <EgorShparovv@yandex.ru>
2025-11-27 11:48:28 +0300

    add push function

--- list/list1.c
+++ list/list1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef int node_type;
 typedef struct Node Node;
@@ -9,6 +10,7 @@ struct Node
 };
 
 void print(Node * list);
+void push(Node ** ptr_list, node_type value);
 
 int main(void)
 {
@@ -22,6 +24,9 @@ int main(void)
 
     print(list);
 
+    push(&list, 10);
+
+    print(list);
     return 0;
 }
 
@@ -30,4 +35,12 @@ void print(Node * list)
     for (Node * ptr = list; ptr != NULL; ptr = ptr->next)
         printf("%d ", ptr->data);
     printf("\n");
+}
+
+void push(Node ** ptr_list, node_type value)
+{
+    Node * ptr = malloc(sizeof(Node));
+    ptr->data = value;
+    ptr->next = *ptr_list;
+    *ptr_list = ptr;
 }
